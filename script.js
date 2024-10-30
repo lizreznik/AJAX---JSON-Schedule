3622bf4afa587bd4e3b55a847683584d4fdca982
lizreznik <525ereznik@frhsd.com>
2024-10-30 10:13:19 -0400

    javascript finished

--- script.js
+++ script.js
@@ -1,51 +1,65 @@
-// JSON File Link: https://api.npoint.io/63a88c84843d6c783c74 //
-const scheduleContainer = $('#scheduleList');
-const btn = $('#submitDay');
+$(document).ready(function () {
 
 
-btn.on('click', function () {
+    // JSON File Link: https://api.npoint.io/63a88c84843d6c783c74 //
+    const scheduleContainer = $('#scheduleList');
+    const btn = $('#submitDay');
 
 
-    //Valdidate the input
-
-    let selectedDay = $('#dayInput').val().trim().toUpperCase()
-    console.log(selectedDay)
-
-    if (!['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(selectedDay)) {
-        alert('You need to type a valid letter day!');
-        return;
-    }
-
-    else {
-        $.ajax({
-            url: 'https://api.npoint.io/63a88c84843d6c783c74',
-            method: 'GET',
-            success: function (data) {
-
-                const schedule = data.schedule
-                const daySchedule = schedule.filter(item => item.days.includes(selectedDay))
-                $("#scheduleList").clear()
-                renderHTML(data)
-            }
-        })
-
-    }
-})
-
-//filter the data to show only the classes that meet on the letter day entered // 
-function renderHTML(data) {
-    var htmlString = ''
-    daySchedule.forEach(function (classItem) {
-        htmlString.append(`
-            <tr>
-                <td>${classItem.period}</td>
-                <td>${classItem.time}</td> 
-                <td>${classItem.class}</td>
-                <td>${classItem.teacher}</td>
-                <td>${classItem.room}</td>
-                }`)
-    });
+    btn.on('click', function () {
 
 
-    scheduleContainer.append(htmlString)
-}
+        //Valdidate the input
+
+        let selectedDay = $('#dayInput').val().trim().toUpperCase()
+        console.log(selectedDay)
+
+        if (!['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(selectedDay)) {
+            alert('You need to type a valid letter day!');
+            return;
+        }
+
+        else {
+            $.ajax({
+                url: 'https://api.npoint.io/63a88c84843d6c783c74',
+                method: 'GET',
+                success: function (data) {
+
+                    const schedule = data.schedule
+                    console.log(schedule)
+                    const daySchedule = schedule.filter(item => item.days.includes(selectedDay))
+                    console.log(daySchedule)
+                    renderHTML(daySchedule)
+
+
+
+
+                }
+            })
+
+        }
+
+        function renderHTML(daySchedule) {
+            $("#scheduleList").empty()
+            var htmlString = ''
+            daySchedule.forEach(function (classItem) {
+
+                htmlString += (`
+                    <tr>
+                    <td>${classItem.period}</td>
+                    <td>${classItem.class}</td>
+                    <td>${classItem.teacher}</td>
+                    <td>${classItem.room}</td>
+                   </tr> `)
+
+            });
+
+            $('#scheduleList').append(htmlString);
+        }
+    })
+
+    //filter the data to show only the classes that meet on the letter day entered // 
+
+
+
+});
